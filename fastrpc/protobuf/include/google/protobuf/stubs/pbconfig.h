fbcc52926e15130c916c8353017a1a63de9df15d
feimat <512284622@qq.com>
2015-06-23 00:25:41 +0800

    change pb 2.4 to pb 3.0

---
+++ fastrpc/protobuf/include/google/protobuf/stubs/pbconfig.h
@@ -0,0 +1,8 @@
+// Note: Google Protobuf internal only. Do NOT include.
+ #define GOOGLE_PROTOBUF_[1;33m[KHASH_MAP_CLASS[m[K unordered_map
+ #define GOOGLE_PROTOBUF_[1;33m[KHASH_MAP_H[m[K <tr1/unordered_map>
+ #define GOOGLE_PROTOBUF_[1;33m[KHASH_NAMESPACE[m[K std::tr1
+ #define GOOGLE_PROTOBUF_[1;33m[KHASH_SET_CLASS[m[K unordered_set
+ #define GOOGLE_PROTOBUF_[1;33m[KHASH_SET_H[m[K <tr1/unordered_set>
+ #define GOOGLE_PROTOBUF_[1;33m[KHAVE_HASH_MAP[m[K 1
+ #define GOOGLE_PROTOBUF_[1;33m[KHAVE_HASH_SET[m[K 1
